a5b8a3e1c80d0d3bc797f6785b378d890c484cfe
HansolKim <hansolkim0810@gmail.com>
2016-08-10 08:57:07 +0900

    16-08-10 activity layour done

--- PeruPHR_App/app/src/main/java/com/app/phr/peru/peruphr_app/JAVA/Login.java
+++ PeruPHR_App/app/src/main/java/com/app/phr/peru/peruphr_app/JAVA/Login.java
@@ -1,9 +1,11 @@
 package com.app.phr.peru.peruphr_app.JAVA;
 
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -60,7 +62,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
     private UserLoginTask mAuthTask = null;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
+    private AutoCompleteTextView mIDView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
@@ -70,7 +72,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        mIDView = (AutoCompleteTextView) findViewById(R.id.id);
         populateAutoComplete();
 
         mPasswordView = (EditText) findViewById(R.id.password);
@@ -86,7 +88,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 //            }
 //        });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.login_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -118,7 +120,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
             return true;
         }
         if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+            Snackbar.make(mIDView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
                     .setAction(android.R.string.ok, new View.OnClickListener() {
                         @Override
                         @TargetApi(Build.VERSION_CODES.M)
@@ -157,11 +159,11 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
         }
 
         // Reset errors.
-        mEmailView.setError(null);
+        mIDView.setError(null);
         mPasswordView.setError(null);
 
         // Store values at the time of the login attempt.
-        String email = mEmailView.getText().toString();
+        String email = mIDView.getText().toString();
         String password = mPasswordView.getText().toString();
 
         boolean cancel = false;
@@ -176,12 +178,12 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
-            mEmailView.setError(getString(R.string.error_field_required));
-            focusView = mEmailView;
+            mIDView.setError(getString(R.string.error_field_required));
+            focusView = mIDView;
             cancel = true;
         } else if (!isEmailValid(email)) {
-            mEmailView.setError(getString(R.string.error_invalid_id));
-            focusView = mEmailView;
+            mIDView.setError(getString(R.string.error_invalid_id));
+            focusView = mIDView;
             cancel = true;
         }
 
@@ -224,7 +226,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
                     show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                    //mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
                 }
             });
 
@@ -284,7 +286,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
                 new ArrayAdapter<>(Login.this,
                         android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
 
-        mEmailView.setAdapter(adapter);
+        mIDView.setAdapter(adapter);
     }
 
 
@@ -304,11 +306,11 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
      */
     public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
 
-        private final String mEmail;
+        private final String mID;
         private final String mPassword;
 
         UserLoginTask(String email, String password) {
-            mEmail = email;
+            mID = email;
             mPassword = password;
         }
 
@@ -325,7 +327,7 @@ public class Login extends AppCompatActivity implements LoaderCallbacks<Cursor> 
 
             for (String credential : DUMMY_CREDENTIALS) {
                 String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
+                if (pieces[0].equals(mID)) {
                     // Account exists, return true if the password matches.
                     return pieces[1].equals(mPassword);
                 }
--- PeruPHR_App/app/src/main/java/com/app/phr/peru/peruphr_app/JAVA/MainTab.java
+++ PeruPHR_App/app/src/main/java/com/app/phr/peru/peruphr_app/JAVA/MainTab.java
@@ -1,5 +1,7 @@
 package com.app.phr.peru.peruphr_app.JAVA;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,6 +10,10 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.app.phr.peru.peruphr_app.Fragment.FragmentMyInfo;
 import com.app.phr.peru.peruphr_app.Fragment.FragmentPHR;
 import com.app.phr.peru.peruphr_app.Fragment.FragmentEducationInfo;
@@ -32,35 +38,72 @@ public class MainTab extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_tab);
 
+        //action bar 대신 toolbar를 사용
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar 에서 action bar의 support를 받아옴
         setSupportActionBar(toolbar);
+        //타이틀 변경
+        getSupportActionBar().setTitle("Peru-PHR");
+        //액션바 컬러 변경
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xB9CDE5));
+        //새로고침 아이콘 표시
+        //getSupportActionBar().setDisplayShowCustomEnabled(true);
+        // 홈 아이콘 표시
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationIcon(R.drawable.icon2);
-
-//        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-//        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(createItemList());
-//        recyclerView.setAdapter(recyclerAdapter);
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
+        //tab레이아웃을 사용함. main_tab.xml과 연결되어 있음
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
         setupTabIcons();
     }
 
+    //tab 아이콘 바꿀 수 있음
     private void setupTabIcons() {
         tabLayout.getTabAt(0).setIcon(tabIcons[0]);
         tabLayout.getTabAt(1).setIcon(tabIcons[1]);
         tabLayout.getTabAt(2).setIcon(tabIcons[2]);
     }
 
-    private void setupViewPager(ViewPager viewPager) {
+    //메뉴 버튼 추가, 아이콘 연결 되어 있음
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+
+    //메뉴 이벤트 처리
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //home 버튼 눌렀을때
+        if (id == android.R.id.home){
+            Intent myAct1 = new Intent(getApplicationContext(), Login.class);
+            Toast.makeText(this, "log out", Toast.LENGTH_SHORT).show();
+            //버튼 눌렀을때 액티비티 초기화
+            myAct1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(myAct1);
+            return true;
+        }
+
+//        //refresh 눌렀을 때
+//        if (id == R.id.action_button) {
+//            Toast.makeText(this, "Refresh success", Toast.LENGTH_SHORT).show();
+//            return true;
+//        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //tab에서 보여줄 탭 fragment 이름
+   private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFrag(new FragmentPHR(), "ONE");
-        adapter.addFrag(new FragmentEducationInfo(), "TWO");
-        adapter.addFrag(new FragmentMyInfo(), "THREE");
+        adapter.addFrag(new FragmentPHR(), "PHR");
+        //adapter.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        adapter.addFrag(new FragmentEducationInfo(), "Education Info");
+        adapter.addFrag(new FragmentMyInfo(), "My Info");
         viewPager.setAdapter(adapter);
     }
 
